47a2ba406a2a7cb3502c76d8b79837d962092d28
Etienne Brodu <etnbrdcom@gmail.com>
2014-12-02 14:32:21 +0100

    reviews

--- src/snippets/cb-seq.js
+++ src/snippets/cb-seq.js
@@ -5,10 +5,11 @@ var arg1 = 'a 1',
 
 my_fn(arg1, function cb1(err, res) { //@\label{lst:cb-seq:cb1}@
   // //@\circled{1}@ ...//@\label{lst:cb-seq:cm1}@
+  var shared_identifier = res + '>>'; //@\label{lst:cb-seq:shared-identifier}@
   console.log(res);
 
   my_fn(arg2, function cb2(err, res) { //@\label{lst:cb-seq:cb2}@
-    console.log(res);
+    console.log(shared_identifier + res);
   }); 
   // //@\circled{2}@ ...//@\label{lst:cb-seq:cm2}@
 });
--- src/snippets/vo-seq.js
+++ src/snippets/vo-seq.js
@@ -1,17 +1,19 @@
 var my_fn = require('./my-fn');
 
 var arg1 = 'b 1',
-    arg2 = 'b 2';
+    arg2 = 'b 2',
+    shared_identifier; //@\label{lst:vo-seq:shared-identifier}@
 
 my_fn(arg1)
-.then(function cb1(err, res) { //@\label{lst:pr-seq:cbdef1}@
-  // //@\circled{1}@ ...//@\label{lst:cb-seq:cm1}@
+.then(function cb1(err, res) { //@\label{lst:vo-seq:cbdef1}@
+  // //@\circled{1}@ ...//@\label{lst:vo-seq:cm1}@
+  shared_identifier = res + '>>';
   console.log(res);
 
-  var v = my_fn(arg2); //@\label{lst:pr-seq:cbdef2}@
-  // //@\circled{2}@ ...//@\label{lst:cb-seq:cm2}@
-  return v; // return the promise from my_fn //@\label{lst:pr-seq:ret}@
+  var v = my_fn(arg2); //@\label{lst:vo-seq:cbdef2}@
+  // //@\circled{2}@ ...//@\label{lst:vo-seq:cm2}@
+  return v; // return the promise from my_fn //@\label{lst:vo-seq:ret}@
 })
 .then(function cb2(err, res) {
-  console.log(res);
+  console.log(shared_identifier + res);
 });
